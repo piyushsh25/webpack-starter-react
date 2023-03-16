159a762b1b253de30081b4494d3d73859c59de00
piyush <piyushsharmashpr@gmail.com>
2023-03-16 10:18:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react"
+export function App() {
+    return <div>
+        <h1>hiiii {new Date().toString()}</h1>
+    </div>
+}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+module.exports = {
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index.bundle.js"
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                        loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
